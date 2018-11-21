6869a99f019a5991bc8e163877e99cdbdc660019
vitams <vmpartner@gmail.com>
2018-11-22 02:03:52 +0500

    Init

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/vmpartner/go-mqdb
---
+++ mqdb.go
@@ -0,0 +1,64 @@
+package pgdb
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
+)
+
+var User string
+var Pass string
+var Host string
+var Port string
+var Name string
+var Ping int
+var Debug bool
+
+var DB *gorm.DB
+var lastPing time.Time
+
+func New() *gorm.DB {
+
+	if Ping <= 0 {
+		Ping = 1
+	}
+
+	if DB == nil {
+		Connect()
+		return New()
+	}
+	if time.Now().After(lastPing.Add(time.Duration(Ping) * time.Minute)) {
+		lastPing = time.Now()
+		err := DB.DB().Ping()
+		if err != nil {
+			DB.Close()
+			Connect()
+			return New()
+		}
+	}
+
+	return DB
+}
+
+func Close() {
+	DB.Close()
+}
+
+func Connect() {
+	dbLink := GetLInk()
+	var err error
+	DB, err = gorm.Open("postgres", dbLink)
+	DB.LogMode(Debug)
+	if err != nil {
+		panic(err.Error())
+	}
+	DB.DB().SetMaxIdleConns(100)
+	DB.DB().SetMaxOpenConns(1000)
+}
+
+func GetLInk() string {
+	dbLink := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", User, Pass, Host, Port, Name)
+
+	return dbLink
+}
